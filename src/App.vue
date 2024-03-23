39ed74f371d880ea15d14c581763134abc5a1479
ernesthenry <henry38ernest@gmail.com>
2024-03-23 22:09:43 +0300

    Refactor code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <AppHead>
-    <title>Vue.js 3 Master Class Forum</title>
+    <title>Forum</title>
     <meta name="description" content="An awesome Vue.js 3 powered forum!">
 
     <!-- Social -->
-    <meta property="og:title" content="Vue.js 3 Master Class Forum">
+    <meta property="og:title" content="Forum">
     <meta property="og:description" content="An Awesome Vue.js 3 powered forum!">
     <meta property="og:image" content="https://vueschool.io/media/f007f6057444d9a7f567163391d2b366/vuejs-3-master-class-not-transparent.jpg">
 
     <!-- Twitter -->
-    <meta name="twitter:title" content="Vue.js 3 Master Class Forum">
+    <meta name="twitter:title" content="Forum">
     <meta name="twitter:description" content="An Awesome Vue.js 3 powered forum!">
     <meta name="twitter:image" content="https://vueschool.io/media/f007f6057444d9a7f567163391d2b366/vuejs-3-master-class-not-transparent.jpg">
     <meta name="twitter:card" content="summary_large_image">
